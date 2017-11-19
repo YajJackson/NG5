7360eea5c7daa6f583153ea1cfa1e724f28e58f8
Jay Jackson <jaynickjackson@gmail.com>
2017-11-19 12:53:26 -0500

    added home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  itemCount: number = 4
+  btnText: string = 'Add Item'
+  goalText: string = 'Create life'
+  goals = []
+
   constructor() { }
 
   ngOnInit() {
+    this.itemCount = this.goals.length
   }
 
+  addItem() {
+    this.goals.push(this.goalText)
+    this.goalText = ''
+    this.itemCount = this.goals.length
+  }
 }
